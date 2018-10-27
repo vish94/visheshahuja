3c4aecc9fade56c36f48f10d3c29a5f0f214ec35
vishesh <vishesh.ahuja@naukri.com>
2018-10-27 19:41:36 +0530

    initial commit to add code

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from kamsolutions.pl/projects/resume/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 07 May 2017 11:20:53 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="Vishesh is a Software Engineer who loves to write PHP, JavaScript, SQL and HTML. He works at Shopclues.com and is an alumni of Delhi College of Engineering" name="description">
+    <meta content="Vishesh, Ahuja, Vishesh Ahuja, software, engineer, php, html, javascript, web development, code, Shopclues, DTU, DCE, Delhi College of Engineering, Delhi Technological University" name="keywords">
+    <title>I'm Vishesh Ahuja</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-98679113-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div id="photo-header" class="text-center">
+            <!-- PHOTO (AVATAR) -->
+            <div id="photo">
+              <img src="img/photo.jpg" alt="avatar">
+            </div>
+            <div id="text-header">
+              <h1>Hello,<br> my name is <span>Vishesh Ahuja</span> and I love to code.</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-7">
+          <!-- ABOUT ME -->
+          <div class="box">
+            <h2>About Me</h2>
+            <p>I am passionate about learning! I believe that one should always move out of the books and study material that the school/college provides and explore the world out of the educational institution. I am a self-motivated person with good communication and inter-personal skills. I graduated from Delhi Technological University (formerly Delhi College of Engineering) in 2016. Currently working as a Software Engineer at Shopclues.com, I am constantly exploring new horizons.</p>
+          </div>
+          <!-- EXPERIENCES -->
+          <div class="box">
+            <h2>Experiences</h2>
+            <div class="job clearfix">
+              <div class="col-xs-3">
+                <div class="where">Shopclues.com</div>
+                <div class="year"> June 2016 - Present</div>
+              </div>
+              <div class="col-xs-9">
+                <div class="profession">Software Engineer</div>
+                <div class="description">Working on projects and feature development of Desktop site, Mobile Site and Admin Panel. PHP, JavaScript, jQuery, AJAX and SQL are used extensively. In my free time I try to develop awesome tools that can be used by me and my team.</div>
+              </div>
+            </div>
+            <div class="job clearfix">
+              <div class="col-xs-3">
+                <div class="where">Axsiom</div>
+                <div class="year">December 2014 - March 2015</div>
+              </div>
+              <div class="col-xs-9">
+                <div class="profession">Web Developer Intern</div>
+                <div class="description">Developed web based tools that were used by the marketing and analytics team extensively to reduce manual tasks. Developed web bots using PHP and SQL which were capable of crawling websites, filling contact forms and retrieving contact information.</div>
+              </div>
+            </div>
+            <div class="job clearfix">
+              <div class="col-xs-3">
+                <div class="where">Genpact</div>
+                <div class="year">June 2014 - July 2014</div>
+              </div>
+              <div class="col-xs-9">
+                <div class="profession">Information Security Intern</div>
+                <div class="description">Assessed applications for software and procedural vulnerabilities to protect them from external threats. Followed OWASP guide and suggested secure coding guidlines to be followed in the organisation.</div>
+              </div>
+            </div>
+          </div>
+          <!-- EDUCATION -->
+          <div class="box">
+            <h2>Education</h2>
+            <ul id="education" class="clearfix">
+              <li>
+                <div class="year pull-left">2016</div>
+                <div class="description pull-right">
+                  <h3>Graduated from Delhi Technological University</h3>
+                  <p>B.Tech Electronics and Communication from Delhi Technological University (formerly Delhi College of Engineering).</p>
+                </div>
+              </li>
+              <li>
+                <div class="year pull-left">2012</div>
+                <div class="description pull-right">
+                  <h3>High School</h3>
+                  <p>Passed out of Venkateshwar International School, Dwarka. Received an acknowledgement letter from Central Board of Secondary Education (CBSE) for the virtue of performance within top 1% of the Senior School Certificate held during the academic session ending March/April 2012.</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-5">
+          <!-- CONTACT -->
+          <div class="box clearfix">
+            <h2>Contact</h2>
+            <div class="contact-item">
+              <div class="icon pull-left text-center"><span class="fa fa-envelope fa-fw"></span></div>
+              <div class="title only pull-right">visheshahuja@gmail.com</div>
+            </div>
+            <a href="https://www.facebook.com/vishesh.ahuja" target="_blank"><div class="contact-item">
+              <div class="icon pull-left text-center"><span class="fa fa-facebook fa-fw"></span></div>
+              <div class="title pull-right">Facebook</div>
+              <div class="description pull-right">https://www.facebook.com/vishesh.ahuja</div>
+            </div></a>
+            <a href="https://www.linkedin.com/in/vishesh-ahuja-3b17a183" target="_blank"><div class="contact-item">
+              <div class="icon pull-left text-center"><span class="fa fa-linkedin fa-fw"></span></div>
+              <div class="title pull-right">Linkedin</div>
+              <div class="description pull-right">Vishesh Ahuja</div>
+            </div></a>
+            <div class="contact-item">
+              <div class="icon pull-left text-center"><span class="fa fa-skype fa-fw"></span></div>
+              <div class="title pull-right">Skype</div>
+              <div class="description pull-right">vishesh_ahuja</div>
+            </div>
+          </div>
+          <!-- SKILLS -->
+          <div class="box">
+            <h2>Skills</h2>
+            <div class="skills">
+              <div class="item-skills" data-percent="0.90">PHP</div>
+              <div class="item-skills" data-percent="0.85">SQL</div>
+              <div class="item-skills" data-percent="0.80">Javascript/jQuery</div>
+              <div class="item-skills" data-percent="0.75">C/C++</div>
+              <div class="item-skills" data-percent="0.60">MVC (framework)</div>
+              <div class="skills-legend clearfix">
+                <div class="legend-left legend">Beginner</div>
+                <div class="legend-left legend"><span>Proficient</span></div>
+                <div class="legend-right legend"><span>Expert</span></div>
+                <div class="legend-right legend">Master</div>
+              </div>
+            </div>
+          </div>
+          <!-- LANGUAGES -->
+          <div class="box">
+            <h2>Other</h2>
+            <div id="language-skills">
+              <div class="skill">PHP <div class="icons pull-right"><div style="width: 95%;" class="icons-red"></div></div></div>
+              <div class="skill">MVC <div class="icons pull-right"><div style="width: 80%;" class="icons-red"></div></div></div>
+              <div class="skill">Restful API <div class="icons pull-right"><div style="width: 75%;" class="icons-red"></div></div></div>
+              <div class="skill">Python <div class="icons pull-right"><div style="width: 50%;" class="icons-red"></div></div></div>
+            </div>
+          </div>
+          <!-- HOBBIES -->
+          <div class="box">
+            <h2>Hobbies</h2>
+            <div class="hobby">Photography</div>
+            <div class="hobby">Gaming</div>
+            <div class="hobby">Programming</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- JQUERY -->
+    <script src="js/jquery.min.js"></script>
+    <!-- BOOTSTRAP -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- SCRIPTS -->
+    <script src="js/scripts.js"></script>
+  </body>
+
+</html>
